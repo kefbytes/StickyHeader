bce4938086f2bacc208f3dbc06fb9414532ed9f4
Kent Franks <kent.franks@lpl.com>
2018-08-22 22:04:46 -0400

    Added customized SegmentedContol

--- KefBytes_StickyHeader/StickyHeaderView.swift
+++ KefBytes_StickyHeader/StickyHeaderView.swift
@@ -27,7 +27,8 @@ class StickyHeaderView: UIView, UISearchBarDelegate {
     @IBOutlet var segmentedControl: UISegmentedControl?
     
     var stickyHeaderDelegate: StickyHeaderDelegate?
-    
+    let buttonBar = UIView()
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
@@ -37,13 +38,46 @@ class StickyHeaderView: UIView, UISearchBarDelegate {
         customizeSearchBar()
     }
 
-    func customizeSegmentedControl() {
-        segmentedControl?.backgroundColor = .lPLLightGray
-    }
-    
+    // MARK: - UI
     func customizeSearchBar() {
         searchBar?.backgroundImage = UIImage()
     }
+    
+    func customizeSegmentedControl() {
+        // create segments
+        segmentedControl?.removeAllSegments()
+        segmentedControl?.insertSegment(withTitle: "Client", at: 0, animated: true)
+        segmentedControl?.insertSegment(withTitle: "Prospect", at: 1, animated: true)
+        segmentedControl?.insertSegment(withTitle: "Local", at: 2, animated: true)
+        segmentedControl?.selectedSegmentIndex = 0
+        
+        // set colors
+        segmentedControl?.backgroundColor = .clear
+        segmentedControl?.tintColor = .clear
+        buttonBar.backgroundColor = UIColor.darkGray
+        
+        // set titles
+        segmentedControl?.setTitleTextAttributes([NSAttributedStringKey.font : UIFont(name: "Helvetica", size: 18) as Any, NSAttributedStringKey.foregroundColor: UIColor.lightGray], for: .normal)
+        segmentedControl?.setTitleTextAttributes([NSAttributedStringKey.font : UIFont(name: "Helvetica", size: 18) as Any, NSAttributedStringKey.foregroundColor: UIColor.darkGray], for: .selected)
+        segmentedControl?.addTarget(self, action: #selector(segmentedControlValueChanged(_:)), for: UIControlEvents.valueChanged)
+        
+        // add as subviews
+        self.addSubview(buttonBar)
+        
+        // Constraints
+        segmentedControl?.translatesAutoresizingMaskIntoConstraints = false
+        buttonBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        segmentedControl?.topAnchor.constraint(equalTo: self.topAnchor)
+        segmentedControl?.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        segmentedControl?.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        buttonBar.topAnchor.constraint(equalTo: (segmentedControl?.bottomAnchor)!).isActive = true
+        buttonBar.heightAnchor.constraint(equalToConstant: 1.5).isActive = true
+        buttonBar.leftAnchor.constraint(equalTo: (segmentedControl?.leftAnchor)!).isActive = true
+        buttonBar.widthAnchor.constraint(equalTo: (segmentedControl?.widthAnchor)!, multiplier: 1 / CGFloat((segmentedControl?.numberOfSegments)!)).isActive = true
+    }
+
     
     // MARK: - UI Calculations
     func setYposition(offset: CGFloat = 0, height: CGFloat = 0, translucent: Bool)  {
@@ -71,6 +105,13 @@ class StickyHeaderView: UIView, UISearchBarDelegate {
     // MARK: - Actions
     @IBAction func segmentControlValueChanged(segmentedControl: UISegmentedControl)  {
         stickyHeaderDelegate?.segmentedControlChanged(withIndex: segmentedControl.selectedSegmentIndex)
+    }
+
+    // MARK: - Selector
+    @objc func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+        UIView.animate(withDuration: 0.3) {
+            self.buttonBar.frame.origin.x = ((self.segmentedControl?.frame.width)! / CGFloat((self.segmentedControl?.numberOfSegments)!)) * CGFloat((self.segmentedControl?.selectedSegmentIndex)!)
+        }
     }
 
     
